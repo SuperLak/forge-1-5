b81ed8be1c247817e4baaf0f08d8dc36d0a19156
Kal <kal.sibbel@gmail.com>
2022-12-16 09:45:45 -0800

    Inital Commit

---
+++ src/main/java/net/kal/tutorialmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.kal.tutorialmod.item;
+
+import net.kal.tutorialmod.TutorialMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
+            () -> new Item(new Item.Properties().isImmuneToFire().group(ItemGroup.MATERIALS)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
