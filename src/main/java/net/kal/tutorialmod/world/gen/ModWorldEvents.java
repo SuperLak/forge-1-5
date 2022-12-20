cb7d587dee923b938c65b90eba8bbaba83796bbf
Kal <kal.sibbel@gmail.com>
2022-12-19 23:03:09 -0800

    Added Ruby Ore Generation

---
+++ src/main/java/net/kal/tutorialmod/world/gen/ModWorldEvents.java
@@ -0,0 +1,15 @@
+package net.kal.tutorialmod.world.gen;
+
+import net.kal.tutorialmod.TutorialMod;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID)
+public class ModWorldEvents {
+
+    @SubscribeEvent
+    public static void biomeLoadingEvent(final BiomeLoadingEvent event) {
+        ModOreGeneration.generateOres(event);
+    }
+}
