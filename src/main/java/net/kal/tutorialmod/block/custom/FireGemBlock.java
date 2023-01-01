8c91e4a3b3e8864a9991bb4713bf2d2b970f41f2
Kal <kal.sibbel@gmail.com>
2023-01-01 10:50:11 -0800

    Removed faulty fire gem block code & added particles to block

--- src/main/java/net/kal/tutorialmod/block/custom/FireGemBlock.java
+++ src/main/java/net/kal/tutorialmod/block/custom/FireGemBlock.java
@@ -2,12 +2,17 @@ package net.kal.tutorialmod.block.custom;
 
 import net.kal.tutorialmod.item.custom.FireGem;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
+import net.minecraft.particles.BlockParticleData;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class FireGemBlock extends Block {
 
@@ -16,8 +21,24 @@ public class FireGemBlock extends Block {
     }
 
     @Override
+    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        float chance = .35f;
+        if (chance < rand.nextFloat()) {
+            worldIn.addParticle(ParticleTypes.FLAME, pos.getX() + rand.nextDouble(),
+                    pos.getY() + 0.5D, pos.getZ() + rand.nextDouble(),
+                    0d, 0.05d, 0d);
+            worldIn.addParticle(new BlockParticleData(ParticleTypes.BLOCK, stateIn), pos.getX() + rand.nextDouble(),
+                    pos.getY() + 0.5D, pos.getZ() + rand.nextDouble(),
+                    0.0D, 0.05D, 0.0D);
+        }
+
+
+        super.animateTick(stateIn, worldIn, pos, rand);
+    }
+
+    @Override
     public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
-        Minecraft.getInstance().player.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, 100));
+
         super.onEntityWalk(worldIn, pos, entityIn);
     }
 }
