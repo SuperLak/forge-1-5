fb7cde999c3e495f9db486bfebce87455f5e3c32
Kal <kal.sibbel@gmail.com>
2023-01-14 13:54:14 -0800

    Added Jei Intergration

---
+++ src/main/java/net/kal/tutorialmod/intergration/jei/TutorialJei.java
@@ -0,0 +1,37 @@
+package net.kal.tutorialmod.intergration.jei;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.kal.tutorialmod.TutorialMod;
+import net.kal.tutorialmod.data.recipes.LightningChannelerRecipe;
+import net.kal.tutorialmod.data.recipes.ModRecipeTypes;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.crafting.RecipeManager;
+import net.minecraft.util.ResourceLocation;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@JeiPlugin
+public class TutorialJei implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(TutorialMod.MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(
+                new LightningChannelerRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        RecipeManager rm = Objects.requireNonNull(Minecraft.getInstance().world).getRecipeManager();
+        registration.addRecipes(rm.getRecipesForType(ModRecipeTypes.LIGHTNING_RECIPE).stream()
+                        .filter(r -> r instanceof LightningChannelerRecipe).collect(Collectors.toList()),
+                LightningChannelerRecipeCategory.UID);
+    }
+}
