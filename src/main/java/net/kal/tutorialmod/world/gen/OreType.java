8c2e20429e145bbbb7b333b4878c201b3f54f357
Kal <kal.sibbel@gmail.com>
2023-03-04 17:52:22 -0800

    Revamped Ore & Entity Generation

--- src/main/java/net/kal/tutorialmod/world/gen/OreType.java
+++ src/main/java/net/kal/tutorialmod/world/gen/OreType.java
@@ -6,19 +6,25 @@ import net.minecraftforge.common.util.Lazy;
 
 public enum OreType {
 
-    RUBY(Lazy.of(ModBlocks.RUBY_ORE), 8, 25, 50),
-    MADRA(Lazy.of(ModBlocks.MADRA_ORE), 6, 8, 24);
+    RUBY(Lazy.of(ModBlocks.RUBY_ORE), 8, 25, 50, 3),
+    MADRA(Lazy.of(ModBlocks.MADRA_ORE), 6, 8, 24, 10);
 
     private final Lazy<Block> block;
     private final int maxVeinSize;
     private final int minHeight;
     private final int maxHeight;
+    private final int veinsPerChunk;
 
-    OreType(Lazy<Block> block, int maxVeinSize, int minHeight, int maxHeight) {
+    OreType(Lazy<Block> block, int maxVeinSize, int minHeight, int maxHeight, int veinsPerChunk) {
         this.block = block;
         this.maxVeinSize = maxVeinSize;
         this.minHeight = minHeight;
         this.maxHeight = maxHeight;
+        this.veinsPerChunk = veinsPerChunk;
+    }
+
+    public int getVeinsPerChunk() {
+        return veinsPerChunk;
     }
 
     public Lazy<Block> getBlock() {
